b516cde9cc62b0653afa96557838b3d9b9bd89a3
DarkXero-dev <steve@techxero.com>
2025-04-21 12:27:02 +0300

    Mirrorlist

--- content/script/xapi.sh
+++ content/script/xapi.sh
@@ -52,7 +52,7 @@ add_chaotic_aur() {
         sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
         sudo pacman-key --lsign-key 3056513887B78AEB
         sudo pacman -U --noconfirm 'https://cdn.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-        sudo pacman -U --noconfirm 'https://cdn.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
         echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
         echo
         echo "Chaotic-AUR Repository added!"
